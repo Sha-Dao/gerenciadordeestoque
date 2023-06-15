c3e5f262eb87fbe53b2d78952370a913ba2e4a2e
Sha-Dao <jp2004.oliveira@gmail.com>
2023-06-15 08:56:21 -0300

    version 5.1

---
+++ GerenciadorDeEstoque/src/controller/TelaLoginController.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controller;
+
+import java.awt.TextField;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+
+/**
+ * FXML Controller class
+ *
+ * @author j_ped
+ */
+public class TelaLoginController implements Initializable {
+
+    @FXML
+    private TextField txtEmail;
+    
+    private PasswordField passwordFieldSenha;
+    
+    @FXML
+    private Button btnEntrar;
+    
+    @FXML
+    private Button btnCadastrar;
+    
+    
+    
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
